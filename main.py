4a30917aa18d80e48fdc8fb31f4e1e50532e0598
Willams Sousa <osdeving@gmail.com>
2025-09-09 13:27:43 -0300

    feat: alterar para chamada em cadeia

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 from llm import get_groq_llm, get_openai_llm, get_ollama_llm
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
-numero_dias = 7
-numero_criancas = 2
-atividade = "praia"
+llm = get_groq_llm()
 
-prompt_template = PromptTemplate(
+prompt = PromptTemplate(
     template="""
     Crie um roteiro de viagem de {dias} dias, 
     para uma família com {numero_criancas} crianças,
     que gostam de {atividade}
-    """
+    """,
+    input_variables=["dias", "numero_criancas", "atividade"]
 )
 
-prompt = prompt_template.format(
-    dias=numero_dias,
-    numero_criancas=numero_criancas,
-    atividade=atividade
-)
+cadeia =  prompt | llm | StrOutputParser()
 
-print("Prompt : \n", prompt)
+print(cadeia.invoke({
+    "dias": 7, 
+    "numero_criancas": 2,
+    "atividade": "praia"
+}))
 
-llm = get_groq_llm()
 
-resposta = llm.invoke(prompt)
-
-print(resposta.content)
 
 
 
